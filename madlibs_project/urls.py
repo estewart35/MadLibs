284ab7474ed45b350228bef0040286b8c00614da
Ethan Stewart <ethan.stewart35@gmail.com>
2025-08-26 20:12:36 -0600

    Add .env and prep for deployment

--- madlibs_project/urls.py
+++ madlibs_project/urls.py
@@ -16,8 +16,12 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
+from django.conf import settings
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
+    # path('admin/', admin.site.urls),
     path('', include('madlibs.urls')),
 ]
+
+if settings.DEBUG:
+    urlpatterns += [path("admin/", admin.site.urls)]
